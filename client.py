741e1061781888498e22f8303c99b7e14996aa5e
UJJWAL PAWAR <ujjwalpawar72@gmail.com>
2020-10-25 10:17:03 +0530

    Update client.py
    Only Helper Functions

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqttClient
 import time
-import ast
 import random
 import math
 import sys
@@ -21,34 +20,27 @@ def distance(curr,to):
 
 
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to broker")
-        global Connected  # Use global variable
-        Connected = True  # Signal connection
-    else:
-        print("Connection failed")
+
 
 def on_message(client, userdata, message):
     #Task-5 Write code here
 
 
-Connected = False  # global variable for the state of the connection
+curr=location_generator()
 #Task-1 Write code here
 client_name=
 
 
-curr=location_generator()
-broker_address = "127.0.0.1"  # Broker address
-port = 1883  # Broker port
+
+
 
 #Task-2 Write code here
-client =   # create new instance
-
-client.on_connect = on_connect  # attach function to callback
-client.on_message = on_message  # attach function to callback
 
 
-client.connect(broker_address, port=port)  # connect to broker
+
+
+
+
 
 
 client.loop_start()  # start the loop
@@ -57,17 +49,17 @@ client.loop_start()  # start the loop
 
 
 
-while Connected != True:  # Wait for connection
-    time.sleep(0.1)
+
 
 end_time=time.time()+15
 while time.time() < end_time:
     # Task-4 Write code here
 
 
+    
+ 
 print("exiting")
-client.disconnect()
-client.loop_stop()
+
 
 print(contact)
 time.sleep(10)
